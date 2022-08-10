2090aa751e3103a1a3f8f0df27c94602db280a79
Matzi24GR <ggeomat@gmail.com>
2022-08-10 03:54:15 +0300

    Change Buttons

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+darkModeEnabled = new Boolean(false);
+darkModeString = $(".darkModeButton:visible").first().text();
+
 function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode")
+  document.body.classList.toggle("dark-mode");
+  darkModeEnabled = !darkModeEnabled;
+  setDarkModeButton();
 }
 
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  darkModeEnabled = true;
   document.body.style.transition = 'none';
-  document.body.classList.toggle("dark-mode")
+  document.body.classList.toggle("dark-mode");
   setTimeout(function() {
     document.body.style.transition = '';
   })
 }
 
 $('[lang="el"]').hide();
+setDarkModeButton();
     
 function toogleLanguage() {
   $('[lang="el"]').toggle();
   $('[lang="en"]').toggle();
+  darkModeString = $(".darkModeButton:visible").text();
+  setDarkModeButton();
+}
+
+function setDarkModeButton() {
+  split = darkModeString.split(" / ")
+  if (darkModeEnabled) {
+    $(".darkModeButton:visible").html(split[0]);
+  } else {
+    $(".darkModeButton:visible").html(split[1]);
+  }
+  
 }
